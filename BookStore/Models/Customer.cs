eded3767e3c642b9b871d34fd33e7850b65c59d1
caglaeraslan@hotmail.com <caglaeraslan@hotmail.com>
2025-04-15 23:49:59 +0300

    odev

---
+++ BookStore/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
+}
